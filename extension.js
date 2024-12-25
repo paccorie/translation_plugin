be7fd622b5445d824160fed53b5392a5d4d4c467
user <vv.naumova@inbox.ru>
2024-12-25 18:14:36 +0300

    name_update

--- extension.js
+++ extension.js
@@ -12,12 +12,12 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "my-extension" is now active!');
+	console.log('Congratulations, your extension "translate_plugin" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('my-extension.translate', function () {
+	const disposable = vscode.commands.registerCommand('translate_plugin.translate', function () {
 		// The code you place here will be executed every time your command is executed
 
 		const editor = vscode.window.activeTextEditor;
